8deb0320285c248b036de7926d869c94a38a768e
vai <vai.sriv@icloud.com>
2025-07-02 15:52:28 -0700

    chore: add syntax highlighting for lua text blocks in nix files

--- .nix/packages/lua/config.nix
+++ .nix/packages/lua/config.nix
@@ -8,25 +8,27 @@
             ./nvim
         ];
 
-        luaConfigRC.myconfig = /* lua */ ''
-            require("options")
-            require("keymaps")
-            require("autocommands")
+        luaConfigRC.myconfig =
+            # lua
+            ''
+                require("options")
+                require("keymaps")
+                require("autocommands")
 
-            local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-            if not (vim.uv or vim.loop).fs_stat(lazypath) then
-                    vim.fn.system({
-                            "git",
-                            "clone",
-                            "--filter=blob:none",
-                            "https://github.com/folke/lazy.nvim.git",
-                            "--branch=stable", -- latest stable release
-                            lazypath,
-                    })
-            end
-            vim.opt.rtp:prepend(lazypath)
+                local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+                if not (vim.uv or vim.loop).fs_stat(lazypath) then
+                        vim.fn.system({
+                                "git",
+                                "clone",
+                                "--filter=blob:none",
+                                "https://github.com/folke/lazy.nvim.git",
+                                "--branch=stable", -- latest stable release
+                                lazypath,
+                        })
+                end
+                vim.opt.rtp:prepend(lazypath)
 
-            require("lazy").setup({ import = "plugins" })
-        '';
+                require("lazy").setup({ import = "plugins" })
+            '';
     };
 }
