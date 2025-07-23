5be40ac6edf2eb3ef20ed05c8af9d0b0a11ba981
vai <vai.sriv@icloud.com>
2025-07-23 14:52:40 -0400

    feat: add autocommand to delete buffers when files are deleted in oil.nvim

--- nix/packages/lua/nvim/lua/autocommands.lua
+++ nix/packages/lua/nvim/lua/autocommands.lua
@@ -8,3 +8,19 @@ vim.api.nvim_create_autocmd("TextYankPost", {
 		vim.highlight.on_yank()
 	end,
 })
+
+-- Delete buffer(s) on corresponding file(s) deletion in oil.nvim (https://github.com/stevearc/oil.nvim/issues/207)
+vim.api.nvim_create_autocmd("User", {
+	desc = "Close buffers when files are deleted in Oil",
+	pattern = "OilActionsPost",
+	callback = function(args)
+		if args.data.err then return end
+		for _, action in ipairs(args.data.actions) do
+			if action.type == "delete" then
+				local _, path = require("oil.util").parse_url(action.url)
+				local bufnr = vim.fn.bufnr(path)
+				if bufnr ~= -1 then vim.cmd.bwipeout { bufnr, bang = true } end
+			end
+		end
+	end,
+})
