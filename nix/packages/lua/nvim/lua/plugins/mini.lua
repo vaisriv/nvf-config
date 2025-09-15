13d53234a574b1376aa6e323ec0ada5eb8756866
vai <vai.sriv@icloud.com>
2025-09-15 12:24:16 -0400

    feat(lua): disable custom mini.statusline location config as default gives more information

--- nix/packages/lua/nvim/lua/plugins/mini.lua
+++ nix/packages/lua/nvim/lua/plugins/mini.lua
@@ -62,9 +62,9 @@ return {
 
 		require("mini.statusline").setup({ use_icons = vim.g.have_nerd_font })
 
-		---@diagnostic disable-next-line: duplicate-set-field
-		require("mini.statusline").section_location = function()
-			return "%2l:%-2v"
-		end
+		-- ---@diagnostic disable-next-line: duplicate-set-field
+		-- require("mini.statusline").section_location = function()
+		-- 	return "%2l:%-2v"
+		-- end
 	end,
 }
