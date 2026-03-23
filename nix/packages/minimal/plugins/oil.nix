f6c493a6a84fbbf5082a2d0464a2e41f0b1282aa
Vai Srivastava <vaisriv@icloud.com>
2026-03-23 22:54:35 +0000

    feat(minimal): override oil command descriptions

--- nix/packages/minimal/plugins/oil.nix
+++ nix/packages/minimal/plugins/oil.nix
@@ -1,9 +1,11 @@
-{ ... }:
+{ lib, ... }:
 {
     vim = {
         utility = {
             oil-nvim = {
                 enable = true;
+
+                gitStatus.enable = true;
                 setupOpts = {
                     columns = [ "icon" ];
                     keymaps = {
@@ -21,11 +23,33 @@
             {
                 key = "<leader>-";
                 action = "<cmd>Oil<cr>";
-                desc = "Open parent directory";
+                desc = "[-] Open parent directory";
                 mode = [
                     "n"
                     "v"
                 ];
+            }
+        ];
+
+        autocmds = [
+            {
+                event = [ "Filetype" ];
+                pattern = [ "oil" ];
+                callback =
+                    lib.generators.mkLuaInline
+                        # lua
+                        ''
+                            function()
+                                require("which-key").add({
+                                    {
+                                        "<leader>q",
+                                        desc = "[Q]uit Oil",
+                                        mode = { "n", "v" },
+                                        buffer = 0,
+                                    },
+                                })
+                            end
+                        '';
             }
         ];
     };
