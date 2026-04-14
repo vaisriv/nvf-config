84c2b4bed99059506e557047d3b5ddf98860d4f2
Vai Srivastava <vaisriv@icloud.com>
2026-04-14 15:19:49 -0400

    feat(vai): add default grammars to treesitter

--- nix/packages/vai/plugins/treesitter.nix
+++ nix/packages/vai/plugins/treesitter.nix
@@ -6,6 +6,8 @@
             fold = false;
             highlight.enable = true;
             indent.enable = false;
+
+            addDefaultGrammars = true;
         };
         # binds.whichKey.register."<leader>ts" = "[T]ree[S]itter";
     };
